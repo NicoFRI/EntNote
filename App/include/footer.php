3fe2597e4baa5259ace8bd83e211ee7bb1b009a5
Catherine-H <titispeed@gmail.com>
2016-04-11 15:07:26 +0200

    probleme appel java

--- App/include/footer.php
+++ App/include/footer.php
@@ -4,11 +4,11 @@
                         <a href="#">Nous contacter !</a></p>
 
     </footer>
-
+ 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="../bootstrap/js/bootstrap.min.js"></script>
     <script src="../bootstrap/js/npm.js"></script>
 
-
+     <script src="/../App/js/script.js"></script>
 </body>
 </html>
---
+++ App/js/script.js
@@ -0,0 +1,17 @@
+(function($) {
+
+  $('#Nav_Module').click(function(e){
+    
+    e.preventDefault();
+
+    /*$('.div_article1').prepend('<h3>Module</h3> <ul class="list-group">');
+
+           $.post( "/../App/bloc/Module.php",
+                  //{ name: "Zara" },
+                  function(data) {
+                     $('.div_article1').append(data);
+                  }
+               );*/
+    });
+    
+})(jQuery);
