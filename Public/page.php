ce2ddc7d5157dd6fabfbfc27dffb0f7b22b1525f
Nicolas0verd3nineji <verdiere.njl@gmail.com>
2016-04-24 16:55:09 +0200

    Retour arriere avant suprresion

---
+++ Public/page.php
@@ -0,0 +1,12 @@
+<?php
+/*  HAUT DE LA PAGE */
+include('/../App/Utilisateur.php');
+include('/../App/include/head.php');
+include('/../App/include/header.php');
+include('/../App/include/nav.php');
+include('/../App/include/bloc.php');
+/*  COEUR DE LA PAGE  */
+
+
+/*  BAS DE LA PAGE*/
+include('/../App/include/footer.php');
