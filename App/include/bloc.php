0c249da879d438527b6f3ec360e7f5471cb0609f
Catherine-H <titispeed@gmail.com>
2016-04-25 14:12:57 +0300

    style

--- App/include/bloc.php
+++ App/include/bloc.php
@@ -9,16 +9,7 @@
                         <div class="thumbnail">
                             <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
                             <div class="caption div_article1">
-                               <!-- <h3>Modules</h3>
-                                <ul class="list-group">
-                                <li class="list-group-item list-group-item-info"><a href="#" >Algorithmique</a></li>
-                                <li class="list-group-item"><a href="#" >Anglais</a></li>
-                                <li class="list-group-item list-group-item-info"><a href="#" >Analyse Numérique</a></li>
-                                <li class="list-group-item"><a href="#" >PHP</a></li>
-                                <li class="list-group-item list-group-item-info"><a href="#" >Java</a></li>
-                                    <li class="list-group-item"><a href="#" >HTML/CSS</a></li>
-                                    <li class="list-group-item list-group-item-info"><a href="#" >Projet</a></li>
-                            </ul>-->
+                               
                             </div>
                         </div>
                     </div>
@@ -30,32 +21,7 @@
                         <div class="thumbnail">
                             <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
                             <div class="caption div_article2">
-                                <!--<h3>Mes interros</h3>
-                                <table class="table table-condensed">
-                                    <tr>
-                                        <td class="info">Type</td>
-                                        <td>Note</td>
-                                        <td class="info">Coefficient</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="info">Interro Java</td>
-                                        <td>14/20</td>
-                                        <td class="info">4</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="info">Interro HTML</td>
-                                        <td>16/20</td>
-                                        <td class="info">2</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="info">Interro PHP</td>
-                                        <td>20/20</td>
-                                        <td class="info">4</td>
-                                    </tr>
-                                </table>
-                                <ul class="list-group">
-                                    <li class="list-group-item list-group-item-info">Moyenne :</li>
-                                </ul>
+                               
                             </div>
                         </div>
                     </div>-->
@@ -67,29 +33,7 @@
                         <div class="thumbnail">
                             <span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>
                             <div class="caption div_article3">
-                                <h3>Détail de l'interro</h3>
-                                <ul class="list-group">
-                                    <li class="list-group-item list-group-item-info"><a href="#" >Interro Java</a></li>
-                                    <li class="list-group-item"><a href="#" >Le: 10/12/2015</a></li>
-                                    <li class="list-group-item list-group-item-info"><a href="#" >14/20</a></li>
-                                    <li class="list-group-item"><a href="#" >Commentaires: </a>
-                                    <p>
-                                        Bien !!
-                                    </p>
-
-                                    </li>
-                                </ul>
-                                <ul class="list-group">
-                                    <li class="list-group-item list-group-item-info"><a href="#" >Interro PHP</a></li>
-                                    <li class="list-group-item"><a href="#" >Le: 20/02/2016</a></li>
-                                    <li class="list-group-item list-group-item-info"><a href="#" >20/20</a></li>
-                                    <li class="list-group-item"><a href="#" >Commentaires: </a>
-                                        <p>
-                                            Très Bien !!
-                                        </p>
-
-                                    </li>
-                                </ul>
+                                
                             </div>
                         </div>
                     </div>
@@ -98,5 +42,4 @@
         </div>
     </section>
 
- 
     
