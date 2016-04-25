f9ddf777f215be62f319213c59bfb3ce376050e8
Nicolas0verd3nineji <verdiere.njl@gmail.com>
2016-04-25 14:53:52 +0200

    Ajout Nav prof

--- App/include/nav.php
+++ App/include/nav.php
@@ -25,8 +25,9 @@ if (empty($_SESSION["Connexion"]["Identifiant"])){
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Que voulez-vous ? <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="">Mes notes</a></li>
                         <li><a id="Nav_Module" href="#">Mes modules</a></li>
+                        <li><a id="Nav_Cours" href="#">Mes cours</a></li>
+                        <li><a id="Nav_Depots" href="#">Mes dépots</a></li>
                     </ul>
                 </li>
             </ul>
@@ -46,3 +47,48 @@ if (empty($_SESSION["Connexion"]["Identifiant"])){
         </div>
     </div>
 </nav>
+
+
+<!--   PROF
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <a class="navbar-brand" href="#">Bienvenue</a>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Que voulez-vous ? <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a id="Nav_Devoirs" href="#">Mes devoirs</a></li>
+                        <li><a id="Nav_Cours" href="#">Mes cours</a></li>
+                        <li><a id="Nav_Depots" href="#">Les dépots</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <a class="navbar-brand" href="#"><?php// echo $util->getNom(); ?></a>
+                <a class="navbar-brand" href="#"><?php// echo $util->getPrenom(); ?></a>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Options <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Plus d'options</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="./../Public/Deconnexion.php">Déconnexion</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+-->
