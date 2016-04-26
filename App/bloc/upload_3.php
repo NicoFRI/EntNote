8ca3dc1285fb50a626573b52bfac1a31ffe8d267
Nicolas0verd3nineji <verdiere.njl@gmail.com>
2016-04-26 10:02:57 +0200

    update upload

--- App/bloc/upload_3.php
+++ App/bloc/upload_3.php
@@ -17,7 +17,8 @@ echo $_FILES['InputFile']['tmp_name'];
 if ( is_uploaded_file($_FILES['InputFile']['tmp_name']) ) {
 	$uid = $_POST['InputNom'];
 	if (move_uploaded_file($_FILES['InputFile']['tmp_name'],
-		'D:/wamp/www/nait/EntNote/document/prof/' . $uid .'.pdf' )) {
-		header('location: D:/wamp/www/nait/EntNote/Public/page.php');
+		'./../../document/prof/'. $uid .'.pdf' )) {
+		header('location: ./../../Public/page.php');
 	};
 }
+
