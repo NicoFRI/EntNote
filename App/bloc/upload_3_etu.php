95db4a9e4c0f8e09b4ee9a595c92525c0b2035ab
Nicolas0verd3nineji <verdiere.njl@gmail.com>
2016-04-26 13:49:28 +0200

    redirection apres upload (eleve)

--- App/bloc/upload_3_etu.php
+++ App/bloc/upload_3_etu.php
@@ -15,7 +15,7 @@ if ( is_uploaded_file($_FILES['InputFile']['tmp_name']) ) {
     $idPath = '/document/etu/'.$uid.'.pdf';
     if (move_uploaded_file($_FILES['InputFile']['tmp_name'],
         './../..'.$idPath )) {
-        //header('location: ./../../Public/page.php');
+        header('location: ./../../Public/page.php');
     };
 }
 $tabINSERT= array ( "Nom_module" => $_POST['NomModule'],
@@ -23,5 +23,3 @@ $tabINSERT= array ( "Nom_module" => $_POST['NomModule'],
                     "idDate_devoir" => $_POST['iddatedev'],
                     "titre_doc" => $_SESSION["Connexion"]["Identifiant"],
                     "ID_typedoc" => '3');
-
-var_dump($tabINSERT);
